22f45234fb633d88dcdea1994d64dc51e18b8f73
tarequr <tarequrr8@gmail.com>
2024-02-01 23:16:17 +0600

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +23,50 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::create([
+            'name'     => 'Tarequr Rahman Sabbir',
+            'email'    => 'superadmin@app.com',
+            'password' => Hash::make('password')
+        ]);
+
+        \App\Models\User::create([
+            'name'     => 'Tayebur Rahman Sabbir',
+            'email'    => 'admin@app.com',
+            'password' => Hash::make('password')
+        ]);
+
+
+        $permissions = [
+            'category.view',
+            'category.create',
+            'category.update',
+            'category.delete',
+
+            'unit.view',
+            'unit.create',
+            'unit.update',
+            'unit.delete',
+        ];
+
+        $permissions = collect($permissions)->map(function ($permission) {
+            return [
+                'name' => $permission,
+                'guard_name' => 'web'
+            ];
+        });
+
+        Permission::insert($permissions->toArray());
+        Role::create(['name' => 'Super Admin'])->givePermissionTo(Permission::all());
+        Role::create(['name' => 'Admin'])->givePermissionTo([
+            'category.view',
+            'category.create',
+            'category.update',
+            'category.delete',
+        ]);
+
+        // User::find(1)->assignRole(['Super Admin','Admin']); //assign multiple role to super admin
+        User::find(1)->assignRole('Super Admin');
+        User::find(2)->assignRole('Admin');
     }
 }
